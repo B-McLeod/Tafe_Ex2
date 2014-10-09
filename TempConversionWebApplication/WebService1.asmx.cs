87de48d85deb1aeb613ddae0852acdda75ab012d
Blake McLeod - TAFE <b-mcleod@outlook.com>
2014-10-09 15:44:57 +1000

    Thursday End-Day

---
+++ TempConversionWebApplication/WebService1.asmx.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Services;
+using TemperatureConversion;
+
+namespace TempConversionWebApplication
+{
+	/// <summary>
+	/// Summary description for WebService1
+	/// </summary>
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+	// [System.Web.Script.Services.ScriptService]
+	public class WebService1 : System.Web.Services.WebService
+	{
+		[WebMethod]
+		public float CelciusToFahrenheit(float temp)
+		{
+			return TempConvert.CelciusToFahrenheit(temp);
+		}
+
+		[WebMethod]
+		public float CelciusToKelvin(float temp)
+		{
+			return TempConvert.CelciusToKelvin(temp);
+		}
+
+		[WebMethod]
+		public float FahrenheitToCelcius(float temp)
+		{
+			return TempConvert.FahrenheitToCelcius(temp);
+		}
+
+		[WebMethod]
+		public float FahrenheitToKelvin(float temp)
+		{
+			return TempConvert.FahrenheitToKelvin(temp);
+		}
+
+		[WebMethod]
+		public float KelvintoCelcius(float temp)
+		{
+			return TempConvert.KelvinToCelcius(temp);
+		}
+
+		[WebMethod]
+		public float KelvinToFahrenheit(float temp)
+		{
+			return TempConvert.KelvinToFahrenheit(temp);
+		}
+	}
+}
--- TemperatureConversion/TempConvert.cs
+++ TemperatureConversion/TempConvert.cs
@@ -4,42 +4,42 @@ namespace TemperatureConversion
 {
 	public class TempConvert
 	{
-		public float CelciusToFahrenheit(float celcius)
+		public static float CelciusToFahrenheit(float celcius)
 		{	// C to F
 			float convertTempUnit = 1.8f;
 			float result = celcius * convertTempUnit + 32f;
 			return result;
 		}
 
-		public float CelciusToKelvin(float celcius)
+		public static float CelciusToKelvin(float celcius)
 		{	// C to K
 			float convertTempUnit = 273.15f;
 			float result = celcius + convertTempUnit;
 			return result;
 		}
 
-		public float FahrenheitToCelcius(float fahrenheit)
+		public static float FahrenheitToCelcius(float fahrenheit)
 		{	// F to C
 			float convertTempUnit = 1.8f;
 			float result = (fahrenheit - 32f) / convertTempUnit;
 			return result;
 		}
 
-		public float FahrenheitToKelvin(float fahrenheit)
+		public static float FahrenheitToKelvin(float fahrenheit)
 		{	// F to K
 			float convertTempUnit = 1.8f;
 			float result = (fahrenheit - 32f) / convertTempUnit + 273.15f;
 			return result;
 		}
 
-		public float KelvinToCelcius(float kelvin)
+		public static float KelvinToCelcius(float kelvin)
 		{	// K to C
 			float convertTempUnit = 273.15f;
 			float result = kelvin - convertTempUnit;
 			return result;
 		}
 
-		public float KelvinToFahrenheit(float kelvin)
+		public static float KelvinToFahrenheit(float kelvin)
 		{	// K to F
 			float convertTempUnit = 1.8f;
 			float result = (kelvin - 273.15f) * convertTempUnit + 32f;
